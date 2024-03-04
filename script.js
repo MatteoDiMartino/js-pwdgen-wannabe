47e2c4bc7d8ae887e910e33f429118d8ca00b01c
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-04 17:41:29 +0100

    feat: add bonus

--- script.js
+++ script.js
@@ -13,8 +13,18 @@
 
     const colorUser = prompt("Inserisci il tuo colore preferito");
     
+// definisco variabile numero
+
+    const numUser = prompt("Inserisci un numero");
+
+// converto variabile numero e sommo
+
+    const totalNum = parseInt(numUser);
+
+    const somNum = 23 + totalNum;
+
 // somma totale col valore numerico
 
-    const passUser = ("la tua password è: " + userName + userSurname + colorUser + "23");
+    const passUser = ("la tua password è: " + userName + userSurname + colorUser + somNum);
     document.getElementById("ps").innerHTML = passUser;
     console.log(passUser);
