88bace968b8c02b5c287868815b5b236210431b7
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-04 14:55:35 +0100

    feat: add html and variables for password generator

---
+++ script.js
@@ -0,0 +1,16 @@
+// definisco variabile nome
+    const userName = prompt("Inserisci il tuo nome");
+
+// definisco variabile cognome
+
+    const userSurname = prompt("Inserisci il tuo cognome");
+
+// definisco variabile colore
+
+    const colorUser = prompt("Inserisci il tuo colore preferito");
+    
+// somma totale col valore numerico
+
+    const passUser = (userName + userSurname + colorUser + "23");
+
+    console.log(passUser);
